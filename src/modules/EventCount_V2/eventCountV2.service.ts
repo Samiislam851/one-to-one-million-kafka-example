fd964580e082053838663b2b2aa0acd6ae0e943d
siam <samisiam851@gmail.com>
2026-04-22 00:53:21 +0600

    v2 planning

--- src/modules/EventCount_V2/eventCountV2.service.ts
+++ src/modules/EventCount_V2/eventCountV2.service.ts
@@ -2,6 +2,16 @@ import { Request, Response } from "express";
 
 export const addClickV2 = async (req: Request, res: Response) =>{
     console.log("🚀 ~ addClickV2 ~ req:", req)
-    
+
+    try {
+        // validate the request body
+        //count up the click in a separate table and add the click event to the event table
+        //if adding event fails rollback the click count
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error
+        })
+    }
 
 }
